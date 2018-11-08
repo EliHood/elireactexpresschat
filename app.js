9c2131f1a40520a4e8872da7633956fc2d9999ae
POWER USER <poweruser@Users-iMac-2.local>
2018-11-07 22:06:54 -0500

    update

--- app.js
+++ app.js
@@ -4,22 +4,13 @@ const  app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 5000;
-const mongoose = require('mongoose');
-const dbRoute = "mongodb://Eli:janemba133@ds155213.mlab.com:55213/elichats"
-const db = mongoose.connection;
 const Chat = require('./config/db');
 const router = express.Router();
 
-router.get("/getData", (req, res) => {
-    Chat.find((err,data) => {
-      if(err) return res.json({success: false, error: err});
-      return res.json({success:true, data:data});
-    });
-
-});
 
 
-mongoose.connect(dbRoute, {useNewUrlParser:true});
+
+// mongoose.connect(dbRoute, {useNewUrlParser:true});
 if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static('client/build'));
